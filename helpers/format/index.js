59734c99591d6064243d60e3cbcefe497e40f1d9
Kevin <brackkevin@gmail.com>
2019-07-23 15:48:49 -0500

    Removed console log

--- helpers/format/index.js
+++ helpers/format/index.js
@@ -5,16 +5,16 @@
 // use trimsearch(arr) or trimsearch(arr, 5)
 // Second argument is optional and will override the
 // max number of results to provide.
-exports.trim =  (arr, limit = 3) => {
+exports.trim = (arr, limit = 3) => {
   // if the array is empty return nothing found
-  console.log("TRIM CALLED")
-  if (arr.length === 0){ 
-    return "No results found for that search" 
+  // console.log("TRIM CALLED");
+  if (arr.length === 0) {
+    return "No results found for that search";
   }
   // Otherwise construct the return statemtne
   let result = "";
-  for(let i = 0; i < arr.length && i <= limit - 1; i++){
+  for (let i = 0; i < arr.length && i <= limit - 1; i++) {
     result += `\n${arr[i].modName}\n${arr[i].data.URL}\n`;
   }
   return result;
-}
+};
