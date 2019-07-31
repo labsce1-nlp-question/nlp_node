da07b82dee1f6bc0e02a77919e817815b1e00c86
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-07-31 18:53:06 -0500

    updated the way the post request is sent to the slack api. Instead we are now using webhooks, this allows us to send responses back to users even if they are in a private channel. Also implemented first steps for taking using feedback with a ephemeral after the links are sent to the user

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express");
 // Creates express app
 const app = express();
 const bodyParser = require("body-parser");
-const request = require("request");
 const axios = require("axios");
 
 // Helpers
@@ -29,7 +28,6 @@ app.use(bodyParser.json());
 app.use("/api/logs", logsRouter);
 
 app.post("/bot", (req, res) => {
-  //console.log(req.body);
 
   const question = { question: req.body.text };
   console.log("BODY: ", req.body);
@@ -52,55 +50,56 @@ app.post("/bot", (req, res) => {
         });
 
       var data = {
-        form: {
-          token: process.env.SLACK_AUTH_TOKEN,
-          channel: req.body.channel_name,
+          response_type:"in_channel",
           text: response.data.matches[0]
             ? `${question.question}\n${trimmed}`
             : "No Results"
-        }
       };
-      request.post("https://slack.com/api/chat.postMessage", data, function(
-        error,
-        response,
-        body
-      ) {
-        // Sends welcome message
-        if (error) {
-          console.log(error);
-        }
-        res.json();
-      });
+      // axios post request to the response_url provided when the user does a slash command and sends a request to this end-point
+      // using the response_url allows us to send back a response to the users channel even if it is private
+      axios.post(req.body.response_url, data)
+        .then(() => {
+          // This sends an empty response to slack, letting slack know we have received the request 
+          res.json();
+          //object used for sending back data to where the request came from. 
+          var ephemeral = {
+            response_type: "ephemeral",
+            attachments: [
+                {
+                  fallback: "If you could read this message, you'd be choosing something fun to do right now.",
+                  callback_id: "feedback_selection",
+                  attachment_type: "default",
+                  actions: [
+                    {
+                      name: "Feedback",
+                      type: "select",
+                      text: "Was this helpful?",
+                      options: [
+                        {
+                          text: "Yes",
+                          value: "True"
+                        },
+                        {
+                          text: "No",
+                          value: "False"
+                        }
+                      ]
+                    }
+                  ]
+                }
+            ]
+          };
+          // After sending the data received from the python search API this will then send a ephemeral
+          axios.post(req.body.response_url, ephemeral).then(res => console.log(res.data));
+        })
+        .catch(err => console.log('error: ', err));
     })
     .catch(err => console.log(err));
 });
 
-// Code for ephemeral
-// Converts the trimmed array into a JSON object, this is needed for the attachments object when sending a 
-// // Ephemeral through the slack API
-// const results = JSON.stringify([{pretext: trimmed}]);
-
-// //console.log('results: ',results)
-// //console.log(response.data.matches)
-
-// // For formating the object for the post request to the slack API please look here https://api.slack.com/docs/message-formatting
-// var data = {
-//     form: {
-//         token: process.env.SLACK_AUTH_TOKEN,
-//         attachments: results,
-//         channel: req.body.channel_name,
-//         text: "Here's what I found ",
-//         user: req.body.user_id
-//     }
-// };
-// request.post('https://slack.com/api/chat.postEphemeral', data, function (error, response, body) {
-//     // Sends welcome message
-//     console.log(response.body)
-//     if (error){
-//         console.log(error);
-//     }
-//     res.json();
-// });
+app.post("/feedback", (req, res) => {
+  console.log("feedback received!", req.body);
+});
 
 app.listen(PORT, function() {
   console.log("Bot is listening on port " + PORT);
