c8660e166d4bc36193e9c0f3f619b97668f1fba0
Kevin <brackkevin@gmail.com>
2019-08-01 17:35:04 -0500

    Made new helper for loging feedback

--- helpers/log/index.js
+++ helpers/log/index.js
@@ -6,3 +6,10 @@ exports.noResult = (data, question) => {
     .then(res => console.log("LOGGED A NON RESPONSE"))
     .catch(err => console.log({err, message: err.message}))
 }
+
+exports.feedback = (question, bot_response, user_response, body) => {
+  db('feedback')
+    .insert({question, bot_response, user_response, body})
+    .then(res => console.log("LOGGED USER FEEDBACK"))
+    .catch(err => console.log({err, message: err.message}))
+}
