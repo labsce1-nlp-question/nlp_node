1ec9b3cd130b2dd639654613a2c8de7fddc9710b
Kevin <brackkevin@gmail.com>
2019-08-01 16:04:06 -0500

    Split trimmed helper into 2 functions

--- helpers/format/index.js
+++ helpers/format/index.js
@@ -6,15 +6,18 @@
 // Second argument is optional and will override the
 // max number of results to provide.
 exports.trim = (arr, limit = 3) => {
-  // if the array is empty return nothing found
-  // console.log("TRIM CALLED");
-  if (arr.length === 0) {
-    return "No results found for that search";
-  }
   // Otherwise construct the return statemtne
-  let result = "";
+  let result = [];
   for (let i = 0; i < arr.length && i <= limit - 1; i++) {
-    result += `\n${arr[i].modName}\n${arr[i].data.URL}\n`;
+    result.push({modName: arr[i].modName, URL: arr[i].data.URL});
   }
   return result;
 };
+
+exports.trimmedString = (arr) => {
+  let result = "";
+  for(let i = 0; i < arr.length; i++) {
+    result += `${i + 1}) ${arr[i].modName}\n${arr[i].URL}\n`
+  }
+  return result;
+}
