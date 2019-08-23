9cefe0b20e261f4438060ced9ac63715ddc8718e
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-08-23 18:03:43 -0500

    removed a console.log

--- api/routers/authrouter.js
+++ api/routers/authrouter.js
@@ -10,7 +10,6 @@ router.get('/redirect', async (req, res) => {
   const uri = `https://slack.com/api/oauth.access?code=${req.query.code}&client_id=${process.env.SLACK_CLIENT_ID}&client_secret=${process.env.SLACK_CLIENT_SECRET}&redirect_uri=${process.env.REDIRECT_URI}`
   
   const reply = await axios.get(uri);
-  console.log(reply)
   // reply has the xoxb token for each slack instance
   if (reply.status !== 200) {
     res.send("Error encountered: \n"+JSON.stringify(reply.statusText)).status(200).end();
