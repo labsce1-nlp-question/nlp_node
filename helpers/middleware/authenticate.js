2754891058795202df031827f7c3c7fd6d265bc2
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-08-28 17:57:21 -0500

    removed undeed folder middleware, not currently using it

--- helpers/middleware/authenticate.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const jwtKey = process.env.JWT_SECRET || 'Please be sure to add a Secret in your env';
-
-function GenerateToken(slack_token, user_id){
-  
-}
