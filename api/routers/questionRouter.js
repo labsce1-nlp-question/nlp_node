ce1e1ee035122819a695f5820685bbbd04903956
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-08-30 15:09:24 -0500

    updated the question end-point to now log empty results as well as record the users history. It now has same functionality as the bot question end-point

--- api/routers/questionRouter.js
+++ api/routers/questionRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const axios = require("axios");
 const format = require("../../helpers/format");
+const log = require("../../helpers/log");
+const userHistoryDB = require("../../data/models/userHistoryDB.js");
 const { authenticate } = require('../../helpers/middleware/authenticate.js');
 
 const SEARCH_URL =
@@ -12,11 +14,20 @@ router.post("/", authenticate, async (req, res) => {
   axios
     .post(`${SEARCH_URL}/qa`, question)
     .then(response => {
+
       if(response.data.length == 0) {
+        const data = {...req.body, user_id: req.decoded.subject };
+
+        log.noResult(data, req.body.question)
+
         res.status(200).json({ message: "No results found" });
       } else {
+        // trim the results from the python api
         const trimmed = format.trim(response.data, 5);
         
+        // Log users question and the Python api response to the database 
+        userHistoryDB.addUserHistory(req.decoded.subject, req.body.question, JSON.stringify(trimmed));
+
         res.status(200).json(trimmed);
       }
     })
